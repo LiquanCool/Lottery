7d041beb18677a8d74d9130385dacb4d7506b41d
BT_4N2_10 <llei0111@bths.edu>
2022-10-21 12:34:19 -0400

    Initial commit

---
+++ src/Lottery.java
@@ -0,0 +1,14 @@
+public class Lottery {
+    /* instance variables */
+    private int lotteryNumber;
+
+    /* constructor */
+    public Lottery()  // no parameters
+// initialize the lotteryNumber in the constructor to a random number
+
+    /* Returns 100, 10, 3, or 0, based on whether userNumber won anything */
+    public int determineWinnings(int userNumber)
+
+// ADD ANY OTHER METHOD(S) YOU THINK MIGHT BE USEFUL
+
+}
---
+++ src/LotteryRunner.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class LotteryRunner {
+    public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int num;
+    System.out.print("Enter your lottery pick: ");
+    num = s.nextLine();
+    if (num == 0 || num == 1|| num == 2 || num == 3 || num == 4 || num == 5 || num == 6 || num == 7 || num == 8 || num == 9)
+    {
+
+    }
+}
